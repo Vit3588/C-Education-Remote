d5f7474435a5539f87ae0ec60afcded35da63aa4
Vitalii Brown <vitaliibrown448899@gmail.com>
2025-01-30 21:07:09 +0300

    User's greeting

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Insert your name: ");
+string username = Console.ReadLine();
+Console.Write("Insert your surname: ");
+string username2 = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
+Console.Write(" ");
+Console.Write(username2);
+Console.Write("!");
