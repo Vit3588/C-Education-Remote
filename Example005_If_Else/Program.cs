447b3a8594c603962d3f6f6443ecc403ba995d04
Vitalii Brown <vitaliibrown448899@gmail.com>
2025-02-24 10:47:27 +0300

    Отдельное приветствие для отдельного пользователя

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "avia")
+{
+    Console.WriteLine("Ура, это же Avia!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
