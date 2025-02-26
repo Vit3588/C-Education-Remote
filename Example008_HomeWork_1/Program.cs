cf5949b4e1d2768dfc8e2ec3e115d1eebad5cacb
Vitalii Brown <vitaliibrown448899@gmail.com>
2025-02-26 20:59:54 +0300

    Возведение числа в квадрат

---
+++ Example008_HomeWork_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Insert the number you want to square!");
+int x = Convert.ToInt32 (Console.ReadLine ());
+int y = x*x;
+Console.Write ("Your number squared is ");
+Console.Write (y);
+Console.Write ("!");
